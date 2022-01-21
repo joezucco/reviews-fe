14fa9070e06587c0e8acfd5b57dee6f8a10c71fd
Joe Zucco <josephzucco@gmail.com>
2022-01-21 10:45:03 -0500

    establish routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+//page & layout components
+import Homepage from './pages/Homepage'
+import ReviewDetails from './pages/ReviewDetails'
+import Category from './pages/Category'
+import SiteHeader from './components/SiteHeader'
+
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <SiteHeader />
+      <Switch>
+        <Route exact path='/'>
+          <Homepage />
+        </Route>
+        <Route path='/details/:id'>
+          <ReviewDetails />
+        </Route>
+        <Route path='/category/:id'>
+          <Category />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App
